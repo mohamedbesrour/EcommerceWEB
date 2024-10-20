31e03a95b0660b6ca1a697075db2202d48283a2f
Besrour Mohamed <m.besrour@yahoo.com>
2024-10-20 08:05:32 +0200

    Navbar + Route Accueil

---
+++ client/src/components/Hero/Hero.tsx
@@ -0,0 +1,42 @@
+// import Image1 from "../../assets/hero/women.png"
+// import Image2 from "../../assets/hero/shopping.png"
+// import Image3 from "../../assets/hero/sale.png"
+// // import Slider from "react-slick";
+
+// const ImageList = [
+//   {
+//     id: 1,
+//     img: Image1,
+//     title: "Upto 50% off on all Men's Wear",
+//     description:
+//       "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//   },
+//   {
+//     id: 2,
+//     img: Image2,
+//     title: "30% off on all Women's Wear",
+//     description:
+//       "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//   },
+//   {
+//     id: 3,
+//     img: Image3,
+//     title: "70% off on all Products Sale",
+//     description:
+//       "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+//   },
+// ];
+
+const Hero = () => {
+  return (
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
+        {/* background pattern */}
+        <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9">
+
+        </div>
+        {/* hero section */}
+    </div>
+  );
+};
+
+export default Hero;
